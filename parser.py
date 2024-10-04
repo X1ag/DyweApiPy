7b1e3c4b405bd7e653a4f81eb624e3b317ab1531
X1ag <super.don17080@gmail.com>
2024-10-04 09:49:58 +0300

    Added price array & added func of clear that array

--- parser.py
+++ parser.py
@@ -1,64 +1,87 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
+import time
 import json
-import pprint
-
+import os
 from get_floor import get_nft_collection_floor
+os.environ['TZ'] = 'Europe/Moscow'
 
-async def calculateOpenPrice(address, timeframe):
-	open_price = 0
-	with open(f'candles/candles{address}{timeframe}.json', 'r') as f:
-		data = json.load(f)
-		pprint.pprint(data)
-	return open_price
-async def calculateHighPrice(address, timeframe):
-	pass
+prices = []
+close_price = None
 
-async def calculateLowPrice(address, timeframe):
-	pass
+def get_seconds_until_next_hour():
+  now = datetime.now()
+  next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+  print(next_hour)
+  return (next_hour - now).total_seconds()
 
-async def calculateClosePrice(address, timeframe):
-	pass
+async def clear_prices_at_start_of_hour():
+  global close_price
+  while True:
+    seconds_until_next_hour = get_seconds_until_next_hour()
+    await asyncio.sleep(seconds_until_next_hour)
+    close_price = prices[-1]
+    prices.clear()
+    print(f'Prices cleared at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    await asyncio.sleep(3600)
+
+async def getPrice(address):
+  result = await get_nft_collection_floor(address)	
+  prices.append(result)
+  if result:
+    return result
+  return None
+
+async def calculateOpenPrice():
+  return prices[0]	
+
+async def calculateHighPrice():
+  return max(prices)	
+
+async def calculateLowPrice():
+  return min(prices)	
+
+async def calculateLastPrice():
+  return close_price 
 
 async def writeFloorInJson(address: str = "EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N", timeframe: str = "1h"):
-		i = 0
-		while True:
-				try:
-						data = {
-								'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S').isoformat(),
-								'price': await get_nft_collection_floor(address),
-								'open': await calculateOpenPrice(address, timeframe),
-								'high': None,
-								'low': None,
-								'close': None,
-						}
-						if data:
-								file_content = ''
+    while True:
+        try:
+            data = {
+                'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'price': await getPrice(address),
+                'open': await calculateOpenPrice(),
+                'high': await calculateHighPrice(),
+                'low': await calculateLowPrice(),
+                'close': await calculateLastPrice()
+            }
+            if data:
+                file_content = ''
 
-								try:
-										with open(f'candles/candles{address}{timeframe}.json', 'r', encoding='utf8') as file:
-												file_content = file.read()
-												if file_content.strip() == '':
-														file_content = '[]'
-								except FileNotFoundError:
-										file_content = '[]'
+                try:
+                    with open(f'candles/candles{address}{timeframe}.json', 'r', encoding='utf8') as file:
+                        file_content = file.read()
+                        if file_content.strip() == '':
+                            file_content = '[]'
+                except FileNotFoundError:
+                    file_content = '[]'
 
-								json_array = json.loads(file_content)
-								json_array.append(data)
+                json_array = json.loads(file_content)
+                json_array.append(data)
 
-								with open(f'candles/candles{address}{timeframe}.json', 'w', encoding='utf8') as file:
-										json.dump(json_array, file, ensure_ascii=False, indent=2)
-										print(f"File candles{address}{timeframe}.json updated")
-										file.write('\n')
+                with open(f'candles/candles{address}{timeframe}.json', 'w', encoding='utf8') as file:
+                    json.dump(json_array, file, ensure_ascii=False, indent=2)
+                    print(f"File candles{address}{timeframe}.json updated")
+                    file.write('\n')
 
-				except Exception as e:
-						print(f"Error: {e}")
-				await asyncio.sleep(5)
-def main(address, timeframe):
-		loop = asyncio.get_event_loop()
-		loop.create_task(writeFloorInJson(address,timeframe))
-		loop.run_forever()
-	
+        except Exception as e:
+            print(f"Error: {e}")
+        await asyncio.sleep(5) 	
+
+async def main(address, timeframe):
+    time.tzset()
+    asyncio.create_task(clear_prices_at_start_of_hour())
+    await writeFloorInJson(address, timeframe)
 
 if __name__ == '__main__':
-		main("EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N", "1h")
+    asyncio.run(main("EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N", "1h"))
