1eea3d47d8514b6f518f16244e420c55f7d87097
X1ag <super.don17080@gmail.com>
2024-10-04 09:48:22 +0300

    Added price array & func of clear that array

--- parser.py
+++ parser.py
@@ -1,45 +1,64 @@
 import asyncio
 from datetime import datetime
 import json
+import pprint
 
 from get_floor import get_nft_collection_floor
 
+async def calculateOpenPrice(address, timeframe):
+	open_price = 0
+	with open(f'candles/candles{address}{timeframe}.json', 'r') as f:
+		data = json.load(f)
+		pprint.pprint(data)
+	return open_price
+async def calculateHighPrice(address, timeframe):
+	pass
+
+async def calculateLowPrice(address, timeframe):
+	pass
+
+async def calculateClosePrice(address, timeframe):
+	pass
 
 async def writeFloorInJson(address: str = "EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N", timeframe: str = "1h"):
-    while True:
-        try:
-            data = {
-                'timestamp': datetime.now().isoformat(),
-                'floor': await get_nft_collection_floor(address)
-            }
-            if data:
-                file_content = ''
+		i = 0
+		while True:
+				try:
+						data = {
+								'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S').isoformat(),
+								'price': await get_nft_collection_floor(address),
+								'open': await calculateOpenPrice(address, timeframe),
+								'high': None,
+								'low': None,
+								'close': None,
+						}
+						if data:
+								file_content = ''
 
-                try:
-                    with open(f'candles/candles{address}{timeframe}.json', 'r', encoding='utf8') as file:
-                        file_content = file.read()
-                        if file_content.strip() == '':
-                            file_content = '[]'
-                except FileNotFoundError:
-                    file_content = '[]'
+								try:
+										with open(f'candles/candles{address}{timeframe}.json', 'r', encoding='utf8') as file:
+												file_content = file.read()
+												if file_content.strip() == '':
+														file_content = '[]'
+								except FileNotFoundError:
+										file_content = '[]'
 
-                json_array = json.loads(file_content)
-                json_array.append(data)
+								json_array = json.loads(file_content)
+								json_array.append(data)
 
-                with open(f'candles/candles{address}{timeframe}.json', 'w', encoding='utf8') as file:
-                    json.dump(json_array, file, ensure_ascii=False, indent=2)
-                    print(f"File candles{address}{timeframe}.json updated")
-                    file.write('\n')
+								with open(f'candles/candles{address}{timeframe}.json', 'w', encoding='utf8') as file:
+										json.dump(json_array, file, ensure_ascii=False, indent=2)
+										print(f"File candles{address}{timeframe}.json updated")
+										file.write('\n')
 
-        except Exception as e:
-            print(f"Error: {e}")
-        await asyncio.sleep(5)
-        
+				except Exception as e:
+						print(f"Error: {e}")
+				await asyncio.sleep(5)
 def main(address, timeframe):
 		loop = asyncio.get_event_loop()
 		loop.create_task(writeFloorInJson(address,timeframe))
 		loop.run_forever()
-  
+	
 
 if __name__ == '__main__':
 		main("EQAOQdwdw8kGftJCSFgOErM1mBjYPe4DBPq8-AhF6vr9si5N", "1h")
